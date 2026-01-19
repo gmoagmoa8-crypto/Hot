5af0c2d297df3d20588b7512e65d734c90bf2acd
gmoagmoa8-crypto <gmoagmoa8@gmail.com>
2026-01-19 12:16:04 +0300

    Create App.jsx file

---
+++ frontend/App.jsx
@@ -0,0 +1,13 @@
+// Main application file
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Welcome to the App</h1>
+        </div>
+    );
+}
+
+export default App;
